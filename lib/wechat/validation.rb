58716c0087b2b7fe495f023fb6ed3e6b815cdf67
Topbit Du <topbit.du@gmail.com>
2016-10-04 10:59:22 +0800

    1, Improve the Validation module to remove the deprecated source codes.

--- lib/wechat/validation.rb
+++ lib/wechat/validation.rb
@@ -4,14 +4,5 @@ require 'wechat/validation/signature'
 
 module Wechat
   module Validation
-
-    def self.sign(nonce, timestamp, token)
-      actual_signature = Digest::SHA1.hexdigest [ nonce, timestamp, token ].sort.join('')
-    end
-
-    class << self
-      deprecate sign: :'Wechat::Validation::Signature.create', deprecator: ActiveSupport::Deprecation.new('1.0', 'wechat-validation')
-    end
-
   end
 end
